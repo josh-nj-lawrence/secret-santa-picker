cc326af9cce228edf1d498b0fb5b633b49623b5a
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-10-09 22:59:55 -0400

    Changed name due to error twine upload was throwing.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="secret-santa-picker",
+    name="secretsantapicker",
     version="0.0.1",
     author="Josh Lawrence",
     author_email="josh.nj.lawrence@gmail.com",
