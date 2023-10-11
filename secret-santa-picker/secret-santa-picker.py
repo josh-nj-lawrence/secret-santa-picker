4a6e8ef473318c6699f8d36687f08e710faa462a
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-10-10 22:25:20 -0400

    WIP

--- secret-santa-picker/secret-santa-picker.py
+++ secret-santa-picker/secret-santa-picker.py
@@ -1,5 +1,6 @@
 import random
 from typing import List
+import copy
 # import email_manager
 
 class Participent:
@@ -7,16 +8,16 @@ class Participent:
     Participent class:
     Define a participent in the secret santa game.
     """
-    def __init__(self, name, email, DontPick = []):
+    def __init__(self, name, email, DontPick: List[str]):
         self.name = name
         self.email = email
-        self.DontPick = []
+        self.DontPick = DontPick
 
     def __eq__(self, other):
         return self.name == other.name and self.email == other.email
     
     def __repr__(self) -> str:
-        return f"Participent({self.name}, {self.email}, {[part.name for part in self.DontPick]})"
+        return f"Participent({self.name}, {self.email}, {[part for part in self.DontPick]})"
 
 class SecretSantaGame:
     """
@@ -26,33 +27,55 @@ class SecretSantaGame:
         self.participents = participents
         self.pairs = [] # Pairs are tuples of (giver, receiver)
 
-    def play_game(self) -> None:
+    def play_game(self, participent_list) -> bool:
         """
         Take participents and pick pairs.
         """
         # Make a copy of the participents list to draw from
-        participents = self.participents[:]
-        for participent in self.participents:
+        participents = copy.deepcopy(participent_list)
+        for participent in participent_list:
             # Pick a random participent from the list
             receiver = random.choice(participents)
             # If the receiver is the same as the giver or the giver is in the receivers DontPick list, pick again
-            while receiver == participent or receiver in participent.DontPick:
+            while receiver == participent or receiver.name in participent.DontPick or participent.name in receiver.DontPick:
+                # If there are no more valid participents to pick from, restart until a valid solution is reached
+                # hat only has last person to draw, 
+                # hat only has 1 person and it's on partipent's DontPick list,
+                # hat has multiple left all in participents dont pick list,
+                # hat has multiple left and it's the partipent and a person on their DontPick list
+                if len(participents) == 1 \
+                    or all(p.name in participent.DontPick for p in participents) \
+                    or all(p.name in ([participent.name] + participent.DontPick) for p in participents):
+                    # Inform user
+                    print("No valid solution found. Restarting...")
+                    # Clear the list of pairs
+                    self.pairs = []
+                    # Restart the game
+                    return False
                 # Redraw case
                 receiver = random.choice(participents)
             # Add participents receiver to the DontPick list for subsequent games if applicable
-            participent.DontPick.append(receiver)
+            participent.DontPick.append(receiver.name)
             # Add participent to receivers DontPick list 
-            receiver.DontPick.append(participent)
+            receiver.DontPick.append(participent.name)
             # Add the pair to the list of pairs
             self.pairs.append((participent, receiver))
             # Remove the receiver from the list of participents
             participents.remove(receiver)
+        return True
 
-    def dry_run(self):
+    def dry_run(self, participent_list):
         """
         Run a dry run and print results to the console instead of sending emails.
         """
-        self.play_game()
+        solution_found = False
+        while not solution_found:
+            participent_copy = copy.deepcopy(participent_list)
+            print("Looking for solution...")
+            solution_found = self.play_game(participent_copy)
+           
+        print("Valid solution found!\n\n")
+        participent_list = participent_copy
         for pair in self.pairs:
             print(f"{pair[0].name} is giving to {pair[1].name}")
 
@@ -64,7 +87,7 @@ if __name__ == "__main__":
     # Example usage
 
     # Create participents and add to list
-    josh = Participent("Josh", "josh.nj.lawrence@gmail.com", ["julee"])
+    josh = Participent("josh", "josh.nj.lawrence@gmail.com", ["julee"])
     julee = Participent("julee", "josh.nj.lawrence@gmail.com", ["josh"])
     mike = Participent("mike", "josh.nj.lawrence@gmail.com", ["leslee"])
     leslee = Participent("leslee", "josh.nj.lawrence@gmail.com", ["mike"])
@@ -76,9 +99,13 @@ if __name__ == "__main__":
     participents = [josh, julee, mike, leslee, kyle, rylan, mitchell, lorin, emilia]
 
     # Create game instance
-    game = SecretSantaGame(participents)
+    secretsanta = SecretSantaGame(participents)
+    bluebox = SecretSantaGame(participents[0:8])
     # Play game
-    game.dry_run()
+    print("\n\nSECRET SANTA DRAW")
+    secretsanta.dry_run(secretsanta.participents)
+    print("\n\nBLUE BOX DRAW")
+    bluebox.dry_run(bluebox.participents)
     # Print participent list after
-    print(participents)
+    #print(participents)
 
